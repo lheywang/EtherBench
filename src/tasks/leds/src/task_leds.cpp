35f494c30eaac61ec8dd1ca3972e08eb92ce9dc3
lheywang <leonard.heywang@proton.me>
2026-03-01 11:18:22 +0100

    Renamed some tasks, and updated arch

---
+++ src/tasks/leds/src/task_leds.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file    idle.cpp
+ * @author  l.heywang <leonard.heywang@proton.me>
+ * @brief   Source file for most simple task, a blinking led, to show
+ * 			the system hasn't crashed (yet ?)
+ * @version 0.1
+ * @date    2026-02-21
+ *
+ * @copyright Copyright (c) 2026
+ *
+ */
+
+// ======================================================================
+//                              INCLUDES
+// ======================================================================
+// Header
+#include "../include/task_leds.h"
+#include "app_threadx.h"
+#include "main.h"
+
+// ======================================================================
+//                              FUNCTIONS
+// ======================================================================
+void leds_task(ULONG arg) {
+  /*
+   * That's just an infinite loop, which blink a led
+   */
+  while (1) {
+    tx_thread_sleep(50);
+    HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
+  }
+}
