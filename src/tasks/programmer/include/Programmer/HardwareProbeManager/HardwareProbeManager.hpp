a919b4058c92c83daa19d0ca8e0dce1dbeff149a
lheywang <leonard.heywang@proton.me>
2026-03-20 20:07:47 +0100

    Added base files for the future programmer class

---
+++ src/tasks/programmer/include/Programmer/HardwareProbeManager/HardwareProbeManager.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file    HardwareProbeManager.hpp
+ * @author  l.heywang <leonard.heywang@proton.me>
+ * @brief   Define the base locking mechanism for the hardware probes as a manager class to handle them.
+ * @version 0.1
+ * @date    2026-03-20
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+#pragma once
+
+// ======================================================================
+//                              INCLUDE
+// ======================================================================
+
+// ======================================================================
+//                               CLASS
+// ======================================================================
---
+++ src/tasks/programmer/src/Programmer/HardwareProbeManager/HardwareProbeManager.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file    HardwareProbeManager.cpp
+ * @author  l.heywang <leonard.heywang@proton.me>
+ * @brief   Implement the base locking mechanism for the hardware probes as a manager class to handle them.
+ * @version 0.1
+ * @date    2026-03-20
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+// ======================================================================
+//                              INCLUDE
+// ======================================================================
+
+// ======================================================================
+//                               CLASS
+// ======================================================================
