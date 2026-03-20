17dc27143609ab693785f4d2f9db82a9928fffe3
lheywang <leonard.heywang@proton.me>
2026-03-20 16:32:47 +0100

    Added full peripherals configuration

---
+++ src/hal/init/include/peripherals/gtzc.h
@@ -0,0 +1,30 @@
+/**
+ * @file    gtzc.h
+ * @author  lheywang (leonard.heywang@proton.me)
+ * @brief
+ * @version 0.1
+ * @date    2026-03-20
+ *
+ * @copyright Copyright (c) 2026
+ *
+ */
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// ======================================================================
+//                              INCLUDE
+// ======================================================================
+#include "main.h"
+
+// ======================================================================
+//                              FUNCTIONS
+// ======================================================================
+
+void MX_GTZC_Init(void);
+
+#ifdef __cplusplus
+}
+#endif
