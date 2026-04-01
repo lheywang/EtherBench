3ee8c952af9e317cbf4f1416df4b59797e9a0292
lheywang <leonard.heywang@proton.me>
2026-04-01 22:15:23 +0200

    Added the ressources files

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,26 @@
+/**
+ * @file    main.cpp
+ * @author  lheywang (leonard.heywang@proton.me)
+ * @brief   Main file, charged to launch the graphical interface.
+ * @version 0.1
+ * @date    2026-04-01
+ *
+ * @copyright Copyright (c) 2026
+ *
+ */
+
+// ----------------------------------------------------------------------
+// INCLUDES
+// ----------------------------------------------------------------------
+// Local libraries
 #include "window.hpp"
+
+// QT
 #include <QApplication>
 
+// ----------------------------------------------------------------------
+// MAIN
+// ----------------------------------------------------------------------
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
---
+++ app/ressources/icons/link.md
@@ -0,0 +1,19 @@
+# Icons
+All icons come from this website : [icons8.com](https://icons8.com)
+
+## List
+Here a complete list of all the icons we did used : 
+
+### App Icon
+
+- [Icon](https://icons8.com/icon/2230/broadcasting)
+
+### Naviguation bar
+
+- [Debugger](https://icons8.com/icon/f103Pm7GmbrD/website-bug)
+- [Home](https://icons8.com/icon/73/home)
+- [Memory](https://icons8.com/icon/bGdonVVR5gP7/smartphone-ram)
+- [IO](https://icons8.com/icon/38392/binary-file)
+- [Programmer](https://icons8.com/icon/DEeZAtE1gIXO/refresh-processor)
+- [Settings](https://icons8.com/icon/364/settings)
+- [Help](https://icons8.com/icon/17077/info-squared)
