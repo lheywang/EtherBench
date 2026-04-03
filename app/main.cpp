f7378e15e6943829f70d2b083d480f530dc3cd37
lheywang <leonard.heywang@proton.me>
2026-04-03 12:21:24 +0200

    Passed the linux build, with the right theme !

--- app/main.cpp
+++ app/main.cpp
@@ -17,15 +17,46 @@
 
 // QT
 #include <QApplication>
+#include <QFile>
+
+// ----------------------------------------------------------------------
+// PROTOTYPES
+// ----------------------------------------------------------------------
+void apply_theme(QApplication &app, const QString &themePath);
 
 // ----------------------------------------------------------------------
 // MAIN
 // ----------------------------------------------------------------------
 int main(int argc, char *argv[]) {
+
+    // Create the app
     QApplication app(argc, argv);
 
+    // Configure style
+    app.setStyle("Fusion");
+    apply_theme(app, ":/styles/dark.qss");
+
+    // Launch the app
     EtherBench::UI::MainWindow win;
     win.show();
 
     return app.exec();
+}
+
+// ----------------------------------------------------------------------
+// FUNCTIONS
+// ----------------------------------------------------------------------
+void apply_theme(QApplication &app, const QString &themePath) {
+
+    // Open and load the file
+    QFile file(themePath);
+
+    if (file.open(QFile::ReadOnly |QFile::Text)) {
+
+        QString styleSheet = QLatin1String(file.readAll());
+        app.setStyleSheet(styleSheet);
+        file.close();
+    } else {
+        qWarning() << "Unable to load the requested stylesheet : " << themePath;
+    }
 }
