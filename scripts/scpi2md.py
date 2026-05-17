738c9e6becdd0f520e775a4b1effbbe889c6a6fb
lheywang <leonard.heywang@proton.me>
2026-05-17 15:47:29 +0200

    Added future doc works

--- scripts/scpi2md.py
+++ scripts/scpi2md.py
@@ -2,5 +2,30 @@
 # Convert the source EtherBenchFirmware commands into the nicely formatted source
 #
 
+# Config path
+# EtherBenchFirmware/src/tasks/parser/config/commands.toml
+
 print("Working...")
 print("Done")
+
+with open("EtherBenchFirmware/src/tasks/parser/config/commands.toml", "r") as f:
+    print(f.readlines())
+
+# Elements lists:
+# Required :
+# description:
+# args : dict
+# returns : dict
+#
+# Optionnal
+# duration: Timings
+# blocking: Y/N
+# image: Insert an image
+# note:
+# tip:
+# important:
+# warning:
+# caution:
+# ex_command:
+# ex_ret:
+# ex_comment:
